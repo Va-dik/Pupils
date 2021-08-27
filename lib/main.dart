63748e1b50e832d8e35d7e6c206c22a399832efa
Va-dik <bin_448@mail.ru>
2021-08-27 23:15:39 +0300

    main

--- lib/main.dart
+++ lib/main.dart
@@ -10,36 +10,71 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
+          backgroundColor: Color(0xFFcdd0fa),
           appBar: AppBar(
-        backgroundColor: Colors.amber[300],
-        title: const Text('Students',
-          style: TextStyle(
-              color: Colors.red,
-              fontSize: 25,
-              fontWeight:FontWeight.w900),
-        ),
-        centerTitle: true,
-        shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(30)),
-        brightness: Brightness.dark,
-      ),
-      body: HomePage()
-      ),
+            backgroundColor: Colors.amber[300],
+            title: Text(
+              'Students',
+              style: TextStyle(
+                  color: Colors.red, fontSize: 25, fontWeight: FontWeight.w900),
+            ),
+            centerTitle: true,
+            shape:
+                RoundedRectangleBorder(borderRadius: BorderRadius.circular(30)),
+            brightness: Brightness.dark,
+          ),
+          body: HomePage()),
     );
   }
 }
 
 class HomePage extends StatefulWidget {
-
   @override
   _HomePageState createState() => _HomePageState();
 }
 
 class _HomePageState extends State<HomePage> {
-  @override
+  int _currentIndex = 0;
+  final List<Widget> _children = [];
+  Color? _bgColor = Colors.red[200];
+  Color _color0 = Colors.red;
+  Color _color1 = Colors.green;
+  Color _color2 = Colors.blue;
 
+  @override
   Widget build(BuildContext context) {
-    return Container(
-      
+    return Scaffold(
+      bottomNavigationBar: BottomNavigationBar(
+        
+        backgroundColor: _bgColor,
+        selectedIconTheme: IconThemeData(color: Colors.blue, size: 35),
+        onTap: _onTabTapped,
+        currentIndex: _currentIndex,
+        items: [
+          BottomNavigationBarItem(icon: Icon(Icons.group, color: _color0,), title: Text('Учащиеся',) ),
+          BottomNavigationBarItem(icon: Icon(Icons.person, color: _color1,), title: Text('Рандоамайзер')),
+          BottomNavigationBarItem(icon: Icon(Icons.chair, color: _color2,), title: Text('Журнал'))
+        ],
+      ),
     );
+  }
+
+  void _onTabTapped(int index) {
+    setState(() {
+      _currentIndex = index;
+
+      if(_currentIndex == 0)
+      {
+        _bgColor = Colors.red[200];
+      }
+      else if(_currentIndex == 1)
+      {
+        _bgColor = Colors.green[200];
+      }
+      else if(_currentIndex == 2)
+      {
+        _bgColor = Colors.blue[200];
+      }
+    });
   }
 }
